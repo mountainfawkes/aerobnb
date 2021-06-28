9ade3e67e3e894a6e08fe96cb50c3d190d02f478
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-06-28 11:20:15 -0700

    adds Nearby section
    Co-authored-by: Tom/Usarneme <jamestcoop@gmail.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,16 +7,19 @@ function Header() {
   return (
     <>
       <Navbar />
-      <div style={{ position: 'relative', display: 'block' }} >
-        <img
-          src={HeroImg}
-          alt='people camping on a lake'
-          style={{ backgroundSize: 'cover', maxWidth: '100vw', position: 'absolute', top: '0', left: '0' }}
-        />
-        <Hero />
+      <div style={{ position: 'relative' }} >
+        <div style={{ position: 'absolute', top: '0', left: '0' }}>
+          <img
+            src={HeroImg}
+            alt='people camping on a lake'
+            style={{ backgroundSize: 'cover', maxWidth: '100vw' }}
+          />
+        </div>
+        <div style={{ position: 'relative' }}>
+          <Hero />
+          <h1>Header</h1>
+        </div>
       </div>
-      <div style={{ position: 'relative', display: 'block' }} ></div>
-      <h1 style={{ position: 'relative' }} >Header</h1>
     </>
   )
 }
---
+++ src/components/Nearby.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Nearby() {
+  return (
+    <>
+      <h2>Explore nearby</h2>
+    </>
+  );
+}
+
+export default Nearby;
